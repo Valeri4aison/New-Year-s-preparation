7a384e03c698448005e7cea59bd5c1786bebd9df
Valeria Vasion <154710663+Valeri4aison@users.noreply.github.com>
2023-12-24 01:37:04 +0200

    Create jewelry exhibition.go

---
+++ jewelry exhibition.go
@@ -0,0 +1,22 @@
+// gift exhibition
+
+func BenchmarkFullAppSimulation(b *testing.B) {
+	config, db, dir, logger, _, err := simapp.SetupSimulation("goleveldb-app-sim", "Simulation")
+	if err != nil {
+		b.Fatalf("simulation setup failed: %s", err.Error())
+	}
+
+	defer func() {
+		db.Close()}}
+
+	for i := 0; i < numSeeds; i++ {
+		config.Seed = rand.Int63()
+
+		for j := 0; j < numTimesToRunPerSeed; j++ {
+			var logger log.Logger
+			if simapp.FlagVerboseValue {
+				logger = log.TestingLogger()
+			} else {
+				logger = log.NewNopLogger()
+			}
+}}
